4cef6829477a0f9f9a986da6278c2b8cbf4b3341
Diego Machado <diego.machadodarosa@gmail.com>
2020-08-07 22:04:06 -0300

    feat: add mobile connection with backend

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -9,9 +9,19 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import api from '../../services/api';
 
 function Landing() {
   const { navigate } = useNavigation();
+  const [totalConnection, setTotalConnection] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnection(total);
+    });
+  }, []);
 
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses');
@@ -50,7 +60,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {' '}
+        Total de {totalConnection} conexões já realizadas {' '}
         <Image source={heartIcon} />
       </Text>
     </View>
